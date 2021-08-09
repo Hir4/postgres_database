194a526a415cedfd048127e2505f36dd740f0419
Rafael Hideki <hirayama.rafael517@gmail.com>
2021-08-09 20:18:10 -0300

    Connect database and select client

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ const client = new Client({
   database: "postgres"
 });
 
+let query = "SELECT * FROM public.clients WHERE first_name LIKE 'Rafael%';";
 
+console.log(query);
+
+client.connect()
+.then(() => console.log("Connected"))
+.then(() => client.query(query))
+.then(results => console.table(results.rows))
+.catch(e => console.log(e))
+.finally(() => client.end())
