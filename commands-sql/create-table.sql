5c7939e3dcfa40599c5b215b81be19bb9e4a307d
Rafael Hideki <hirayama.rafael517@gmail.com>
2021-08-20 14:16:13 -0300

    Query export from DbDesigner

--- commands-sql/create-table.sql
+++ commands-sql/create-table.sql
@@ -1,8 +1,6 @@
-------------------------------------------------------------------------
--- CREATE TABLE
-------------------------------------------------------------------------
-
--- CLIENT TABLE
+----------------------------------------------------------
+-- EXPORTED FROM DbDesigner (DATABASE: PostgreSQL)
+----------------------------------------------------------
 
 CREATE TABLE "public.clients" (
 	"id" serial NOT NULL,
@@ -18,14 +16,14 @@ CREATE TABLE "public.clients" (
 	"phone_ddd" varchar(2) NOT NULL,
 	"phone_number" varchar(10) NOT NULL,
 	"creation_date" TIMESTAMP NOT NULL,
-	"update_date" timestamp NOT NULL,
-	"delete_date" timestamp NOT NULL,
+	"update_date" DATETIME NOT NULL,
+	"delete_date" DATETIME NOT NULL,
 	CONSTRAINT "clients_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
--- PRODUCTS TABLE
+
 
 CREATE TABLE "public.products" (
 	"id" serial NOT NULL,
@@ -35,27 +33,27 @@ CREATE TABLE "public.products" (
 	"product_quantity" integer NOT NULL,
 	"product_price" FLOAT NOT NULL,
 	"creation_date" TIMESTAMP NOT NULL,
-	"update_date" timestamp NOT NULL,
-	"delete_date" timestamp NOT NULL,
+	"update_date" DATETIME NOT NULL,
+	"delete_date" DATETIME NOT NULL,
 	CONSTRAINT "products_pk" PRIMARY KEY ("id","group_id")
 ) WITH (
   OIDS=FALSE
 );
 
--- PRODUCTS GROUP TABLE
+
 
 CREATE TABLE "public.product_group" (
 	"id" serial NOT NULL,
 	"product_type" varchar(30) NOT NULL UNIQUE,
 	"creation_date" TIMESTAMP NOT NULL,
-	"update_date" timestamp NOT NULL,
-	"delete_date" timestamp NOT NULL,
+	"update_date" DATETIME NOT NULL,
+	"delete_date" DATETIME NOT NULL,
 	CONSTRAINT "product_group_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
--- PROVIDER TABLE
+
 
 CREATE TABLE "public.provider" (
 	"id" serial NOT NULL,
@@ -70,53 +68,53 @@ CREATE TABLE "public.provider" (
 	"phone_ddd" varchar(2) NOT NULL,
 	"phone_number" varchar(10) NOT NULL,
 	"creation_date" TIMESTAMP NOT NULL,
-	"update_date" timestamp NOT NULL,
-	"delete_date" timestamp NOT NULL,
+	"update_date" DATETIME NOT NULL,
+	"delete_date" DATETIME NOT NULL,
 	CONSTRAINT "provider_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
--- SALE HEADER TABLE
+
 
 CREATE TABLE "public.sale_header" (
 	"id" serial NOT NULL,
 	"client_id" integer NOT NULL,
 	"total_bought" FLOAT NOT NULL,
-	"pay_method" varchar(7) NOT NULL,
-	"installments" integer NOT NULL DEFAULT '1',
-	"due_date" timestamp NOT NULL,
+	"pay_method" integer NOT NULL,
+	"installments" integer(2) NOT NULL DEFAULT '1',
+	"due_date" DATETIME NOT NULL,
 	"shipping" FLOAT NOT NULL DEFAULT '0',
-	"delivery_time" timestamp NOT NULL,
+	"delivery_time" DATETIME NOT NULL,
 	"confirmation" BOOLEAN NOT NULL,
 	"creation_date" TIMESTAMP NOT NULL,
-	"update_date" timestamp NOT NULL,
-	"delete_date" timestamp NOT NULL,
+	"update_date" DATETIME NOT NULL,
+	"delete_date" DATETIME NOT NULL,
 	CONSTRAINT "sale_header_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
--- SALE ITENS TABLE
+
 
 CREATE TABLE "public.sale_itens" (
 	"sale_header_id" serial NOT NULL,
 	"line_id" integer NOT NULL,
 	"product_id" integer NOT NULL,
 	"product_quantity" integer NOT NULL,
-	"update_date" timestamp NOT NULL,
-	"delete_date" timestamp NOT NULL,
+	"update_date" DATETIME NOT NULL,
+	"delete_date" DATETIME NOT NULL,
 	CONSTRAINT "sale_itens_pk" PRIMARY KEY ("sale_header_id","line_id","product_id")
 ) WITH (
   OIDS=FALSE
 );
 
--- PAY BILLS TABLE
+
 
 CREATE TABLE "public.pay_bills" (
 	"id" integer NOT NULL,
 	"provider_id" integer NOT NULL,
-	"installments" integer NOT NULL DEFAULT '1',
+	"installments" integer(2) NOT NULL DEFAULT '1',
 	"product_bought" varchar(30) NOT NULL,
 	"product_price" FLOAT NOT NULL,
 	"product_quantity" integer NOT NULL,
@@ -125,32 +123,32 @@ CREATE TABLE "public.pay_bills" (
 	"shipping" FLOAT,
 	"confirmation" BOOLEAN NOT NULL,
 	"creation_date" TIMESTAMP NOT NULL,
-	"update_date" timestamp NOT NULL,
-	"delete_date" timestamp NOT NULL,
+	"update_date" DATETIME NOT NULL,
+	"delete_date" DATETIME NOT NULL,
 	CONSTRAINT "pay_bills_pk" PRIMARY KEY ("id","provider_id","installments")
 ) WITH (
   OIDS=FALSE
 );
 
--- RECEIVE BILLS TABLE
+
 
 CREATE TABLE "public.receive_bills" (
 	"client_id" integer NOT NULL,
 	"sale_header_id" integer NOT NULL,
-	"installments" integer NOT NULL DEFAULT '1',
+	"installments" integer(2) NOT NULL DEFAULT '1',
 	"value" FLOAT NOT NULL,
 	"pay_method" varchar(7) NOT NULL,
-	"due_date" timestamp NOT NULL,
+	"due_date" DATETIME NOT NULL,
 	"confirmation" BOOLEAN NOT NULL,
 	"creation_date" TIMESTAMP NOT NULL,
-	"update_date" timestamp NOT NULL,
-	"delete_data" timestamp NOT NULL,
+	"update_date" DATETIME NOT NULL,
+	"delete_data" DATETIME NOT NULL,
 	CONSTRAINT "receive_bills_pk" PRIMARY KEY ("client_id","sale_header_id","installments")
 ) WITH (
   OIDS=FALSE
 );
 
--- ADMIN TABLE
+
 
 CREATE TABLE "public.admin" (
 	"id" serial NOT NULL,
@@ -166,14 +164,14 @@ CREATE TABLE "public.admin" (
 	"phone_ddd" varchar(2) NOT NULL,
 	"phone_number" varchar(2) NOT NULL,
 	"creation_date" TIMESTAMP NOT NULL,
-	"update_date" timestamp NOT NULL,
-	"delete_date" timestamp NOT NULL,
+	"update_date" DATETIME NOT NULL,
+	"delete_date" DATETIME NOT NULL,
 	CONSTRAINT "admin_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
--- EMPLOYEE TABLE
+
 
 CREATE TABLE "public.employee" (
 	"id" serial NOT NULL,
@@ -189,20 +187,36 @@ CREATE TABLE "public.employee" (
 	"phone_ddd" varchar(2) NOT NULL,
 	"phone_number" varchar(2) NOT NULL,
 	"creation_date" TIMESTAMP NOT NULL,
-	"update_date" timestamp NOT NULL,
-	"delete_date" timestamp NOT NULL,
+	"update_date" DATETIME NOT NULL,
+	"delete_date" DATETIME NOT NULL,
 	CONSTRAINT "employee_pk" PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
 );
 
--- CONSTRAINTS
+
+
+CREATE TABLE "public.pay_method" (
+	"id" serial NOT NULL,
+	"method_type" varchar(30) NOT NULL UNIQUE,
+	"installments_accept" integer(2) NOT NULL DEFAULT '1',
+	"creation_date" TIMESTAMP NOT NULL,
+	"update_date" DATETIME,
+	"delete_date" DATETIME,
+	CONSTRAINT "pay_method_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
 
 ALTER TABLE "products" ADD CONSTRAINT "products_fk0" FOREIGN KEY ("group_id") REFERENCES "product_group"("id");
 
 
 
 ALTER TABLE "sale_header" ADD CONSTRAINT "sale_header_fk0" FOREIGN KEY ("client_id") REFERENCES "clients"("id");
+ALTER TABLE "sale_header" ADD CONSTRAINT "sale_header_fk1" FOREIGN KEY ("pay_method") REFERENCES "pay_method"("id");
 
 ALTER TABLE "sale_itens" ADD CONSTRAINT "sale_itens_fk0" FOREIGN KEY ("sale_header_id") REFERENCES "sale_header"("id");
 ALTER TABLE "sale_itens" ADD CONSTRAINT "sale_itens_fk1" FOREIGN KEY ("product_id") REFERENCES "products"("id");
@@ -211,3 +225,23 @@ ALTER TABLE "pay_bills" ADD CONSTRAINT "pay_bills_fk0" FOREIGN KEY ("provider_id
 
 ALTER TABLE "receive_bills" ADD CONSTRAINT "receive_bills_fk0" FOREIGN KEY ("client_id") REFERENCES "clients"("id");
 ALTER TABLE "receive_bills" ADD CONSTRAINT "receive_bills_fk1" FOREIGN KEY ("sale_header_id") REFERENCES "sale_header"("id");
+
+ALTER TABLE "admin" ADD CONSTRAINT "admin_fk0" FOREIGN KEY ("creation_date") REFERENCES "users"("creation_date");
+ALTER TABLE "admin" ADD CONSTRAINT "admin_fk1" FOREIGN KEY ("update_date") REFERENCES "users"("update_date");
+ALTER TABLE "admin" ADD CONSTRAINT "admin_fk2" FOREIGN KEY ("delete_date") REFERENCES "users"("delete_date");
+
+ALTER TABLE "employee" ADD CONSTRAINT "employee_fk0" FOREIGN KEY ("creation_date") REFERENCES "users"("creation_date");
+ALTER TABLE "employee" ADD CONSTRAINT "employee_fk1" FOREIGN KEY ("update_date") REFERENCES "users"("update_date");
+ALTER TABLE "employee" ADD CONSTRAINT "employee_fk2" FOREIGN KEY ("delete_date") REFERENCES "users"("delete_date");
+
+
+
+
+
+
+
+
+
+
+
+
