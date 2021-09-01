d77b30831fb1a74f795ea0594e4f5cb1fc87a7ef
Rafael Hideki <hirayama.rafael517@gmail.com>
2021-09-01 19:10:50 -0300

    Update view sale

--- views/view_sale.sql
+++ views/view_sale.sql
@@ -1,16 +1,19 @@
 CREATE OR REPLACE VIEW view_sale 
  	AS SELECT  
 		client_id, 
-		first_name, 
+		first_name,
+		product_name,
 		sale.creation_date, 
 		total_bought, 
-		pay_method,
-		method_type,
-		installments_accept
+		payment.method_type
 	FROM 
 		public.sale_header AS sale
   	INNER JOIN public.clients AS client
 		ON sale.client_id = client.id
+	INNER JOIN public.sale_itens AS sale_itens
+		ON sale_itens.sale_header_id = sale.id
+	INNER JOIN public.products AS products
+		ON products.id = sale_itens.product_id
 	INNER JOIN public.pay_method AS payment
 		ON payment.id = pay_method
  	WHERE 
